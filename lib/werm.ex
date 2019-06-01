befe00c7f1ec217c8e68fa2d779081743bd18498
Francis Karlo Catindig Escota <karlo.escota@gmail.com>
2019-06-01 19:56:18 +0800

    implement item attrs fetcher

--- lib/werm.ex
+++ lib/werm.ex
@@ -1,29 +1,28 @@
 defmodule Werm do
-  @moduledoc """
-  Documentation for Werm.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Werm.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
   def crawl do
     url = "https://sg.carousell.com/categories/electronics-7/?sort_by=time_created"
 
     case HTTPoison.get(url) do
-      {:ok, %HTTPoison.Response{body: body}} ->
-        IO.inspect body
+      {:ok, %HTTPoison.Response{body: html}} ->
+        html
+        |> get_items()
+
       {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect reason
+        IO.inspect(reason)
     end
+  end
+
+  def get_items(html) do
+    Floki.find(html, "div.ep-U")
+  end
+
+  def get_seller_id(item) do
+    Floki.find(item, "div.ep-A")
+    |> Floki.text()
+  end
+
+  def get_item_context(item) do
+    {_, _, context} = Floki.find(item, "div.ep-k") |> List.first()
+    Enum.map(context, &(&1 |> Floki.text()))
   end
 end
--- test/werm_test.exs
+++ test/werm_test.exs
@@ -1,8 +1,36 @@
 defmodule WermTest do
   use ExUnit.Case
-  doctest Werm
 
-  test "greets the world" do
-    assert Werm.hello() == :world
+  describe ".get_items/1" do
+    test "returns the item list" do
+      html = "<html><div class=\"ep-U\"></div><div class=\"ep-U\"></div></html>"
+
+      assert Werm.get_items(html) == [
+               {"div", [{"class", "ep-U"}], []},
+               {"div", [{"class", "ep-U"}], []}
+             ]
+    end
+  end
+
+  describe ".get_seller_id/1" do
+    test "returns the seller id" do
+      item = [{"div", [{"class", "ep-A"}], ["DummySeller"]}]
+      item_html = {"div", [], item}
+
+      assert Werm.get_seller_id(item_html) == "DummySeller"
+    end
+  end
+
+  describe ".get_item_context/1" do
+    test "returns the price, description, and state of the item as a list" do
+      price = {"div", [], ["$10"]}
+      desc = {"div", [], ["Description"]}
+      state = {"div", [], ["Used"]}
+
+      item = [{"div", [{"class", "ep-k"}], [price, desc, state]}]
+      item_html = {"div", [], item}
+
+      assert Werm.get_item_context(item_html) == ["$10", "Description", "Used"]
+    end
   end
 end
