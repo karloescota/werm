18cd127b73fe5ccc203ec9bae36a8940627e0ef5
Francis Karlo Catindig Escota <karlo.escota@gmail.com>
2019-06-01 18:25:15 +0800

    get html

--- lib/werm.ex
+++ lib/werm.ex
@@ -15,4 +15,15 @@ defmodule Werm do
   def hello do
     :world
   end
+
+  def crawl do
+    url = "https://sg.carousell.com/categories/electronics-7/?sort_by=time_created"
+
+    case HTTPoison.get(url) do
+      {:ok, %HTTPoison.Response{body: body}} ->
+        IO.inspect body
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect reason
+    end
+  end
 end
