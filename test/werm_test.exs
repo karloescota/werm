1c0b0a898c57fc07e57b6c1cba6a28e5a0b538e0
Francis Karlo Catindig Escota <karlo.escota@gmail.com>
2019-06-01 20:33:21 +0800

    simplify test

--- test/werm_test.exs
+++ test/werm_test.exs
@@ -4,7 +4,6 @@ defmodule WermTest do
   describe ".get_items/1" do
     test "returns the item list" do
       html = "<html><div class=\"ep-U\"></div><div class=\"ep-U\"></div></html>"
-
       assert Werm.get_items(html) == [
                {"div", [{"class", "ep-U"}], []},
                {"div", [{"class", "ep-U"}], []}
@@ -14,32 +13,38 @@ defmodule WermTest do
 
   describe ".get_seller_id/1" do
     test "returns the seller id" do
-      seller_id = {"div", [{"class", "ep-A"}], ["DummySeller"]}
-      item = {"div", [], [seller_id]}
-
+      item = {"div", [], [seller_id("DummySeller")]}
       assert Werm.get_seller_id(item) == "DummySeller"
     end
   end
 
   describe ".get_item_context/1" do
     test "returns the price, description, and state of the item as a list" do
-      price = {"div", [], ["$10"]}
-      desc = {"div", [], ["Description"]}
-      state = {"div", [], ["Used"]}
-
-      context = {"div", [{"class", "ep-k"}], [price, desc, state]}
-      item = {"div", [], [context]}
-
+      item = {"div", [], [context("$10", "Description", "Used")]}
       assert Werm.get_item_context(item) == ["$10", "Description", "Used"]
     end
   end
 
   describe ".get_item_link/1" do
     test "returns the item link" do
-      link = {"a", [{"class", "ep-ab"}, {"href", "https://item-link"}], []}
-      item = {"div", [], [link]}
-
+      item = {"div", [], [link("https://item-link")]}
       assert Werm.get_item_link(item) == "https://item-link"
     end
+  end
+
+  def seller_id(id) do
+    {"div", [{"class", "ep-A"}], [id]}
+  end
+
+  def link(ref) do
+    {"a", [{"class", "ep-ab"}, {"href", ref}], []}
+  end
+
+  def div(text) do
+    {"div", [], [text]}
+  end
+
+  def context(price, desc, state) do
+    {"div", [{"class", "ep-k"}], [div(price), div(desc), div(state)]}
   end
 end
